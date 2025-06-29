3a2986e785ec342008fcbbda622a750168a3205f
PedroNJorge <pedro.nobre.jorge@gmail.com>
2025-06-29 05:35:25 +0100

    general config

---
+++ config/__init__.py
@@ -0,0 +1,5 @@
+from .redis_config import REDIS_HOST
+from .redis_config import REDIS_PORT
+from .redis_config import PROD_DB
+from .redis_config import TEST_DB
+from .redis_config import TEMP_DB
---
+++ config/redis_config.py
@@ -0,0 +1,6 @@
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+
+PROD_DB = 0  # production db
+TEST_DB = 1  # test-only db
+TEMP_DB = 2  # temp face scans
