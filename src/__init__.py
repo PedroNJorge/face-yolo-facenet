c81fe8a18ceb0a38853f1fa32a6ae228a204861a
PedroNJorge <pedro.nobre.jorge@gmail.com>
2025-06-29 05:34:34 +0100

    init files

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from .detection import YOLODetector
+from .detection import FaceExtractor
+from .recognition import FaceMatcher
+from .recognition import FaceEncoder
+from .database import RedisClient
