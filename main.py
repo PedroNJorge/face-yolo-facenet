3cf665da49c4f124230960fead1344a0f3f3b7b9
PedroNJorge <pedro.nobre.jorge@gmail.com>
2025-07-14 02:55:15 +0100

    main file to be executed

---
+++ main.py
@@ -0,0 +1,27 @@
+import cv2
+from src import Processor, FaceExtractor, FaceEncoder
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(0)  # Webcam
+    pipeline = Processor(FaceExtractor(), FaceEncoder())
+
+    Q_KEY = ord('q')
+    D_KEY = ord('d')
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        cv2.imshow('Face Recognition', frame)
+
+        key = cv2.waitKey(1)
+
+        if key == Q_KEY:
+            break
+        elif key == D_KEY:
+            print("Processing frame")
+            pipeline.process_frame(frame)
+            print("Done!")
+
+    cap.release()
+    cv2.destroyAllWindows()
